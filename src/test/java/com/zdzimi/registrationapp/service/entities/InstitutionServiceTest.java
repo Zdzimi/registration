df75c1cdde196650efdf48de3eb62a46550f0b65
luke <lukjab@yahoo.pl>
2020-09-21 20:32:50 +0200

    master: LJA - added unit test template for InstitutionService

---
+++ src/test/java/com/zdzimi/registrationapp/service/entities/InstitutionServiceTest.java
@@ -0,0 +1,12 @@
+package com.zdzimi.registrationapp.service.entities;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InstitutionServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
